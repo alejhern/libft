c965b0e2cab8fc252e2115e08c7b40353bcb8165
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-01-15 11:50:26 +0100

    style: format ft_strappend function for consistency and readability | norminette

--- ft_strappend.c
+++ ft_strappend.c
@@ -10,31 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft.h"
+#include "libft.h"
 
-char    *ft_strappend(char *dest, char *to_append)
+char	*ft_strappend(char *dest, char *to_append)
 {
-    size_t  dest_len;
-    size_t  to_append_len;
-    char    *new_str;
+	size_t	dest_len;
+	size_t	to_append_len;
+	char	*new_str;
 
-    if (!dest && !to_append)
-        return (NULL);
-    if (!to_append)
-        return (dest);
-    if (!dest)
-        return (ft_strdup(to_append));
-    dest_len = ft_strlen(dest);
-    to_append_len = ft_strlen(to_append);
-    new_str = (char *) malloc(dest_len + to_append_len + 1 * sizeof(char));
-    if (!new_str)
-    {
-        free(dest);
-        return (NULL);
-    }
-    ft_strlcpy(new_str, dest, dest_len + 1);
-    free(dest);
-    ft_strlcat(new_str, to_append, dest_len + to_append_len + 1);
-    new_str[dest_len + to_append_len] = '\0';
-    return (new_str);
+	if (!dest && !to_append)
+		return (NULL);
+	if (!to_append)
+		return (dest);
+	if (!dest)
+		return (ft_strdup(to_append));
+	dest_len = ft_strlen(dest);
+	to_append_len = ft_strlen(to_append);
+	new_str = (char *)malloc(dest_len + to_append_len + 1 * sizeof(char));
+	if (!new_str)
+	{
+		free(dest);
+		return (NULL);
+	}
+	ft_strlcpy(new_str, dest, dest_len + 1);
+	free(dest);
+	ft_strlcat(new_str, to_append, dest_len + to_append_len + 1);
+	new_str[dest_len + to_append_len] = '\0';
+	return (new_str);
 }
