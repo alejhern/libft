772ca2721b4e76fd17ddd36237c7058f82b4be3d
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-03-24 17:21:10 +0100

    fix: add null pointer checks in ft_strncmp function

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,9 +16,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	index;
 
-	index = 0;
-	if (n == 0)
+	if (!s1 || !s2 || n == 0)
 		return (0);
+	index = 0;
 	while (s1[index] && s2[index] && s1[index] == s2[index] && index < n - 1)
 		index++;
 	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
