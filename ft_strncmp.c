ed116ec3b38d7bf31dbcd70cdab9ed902f643801
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-06-01 00:00:23 +0200

    fix: add ft_strcmp function and improve null checks in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,12 +12,28 @@
 
 #include "libft.h"
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	index;
+
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 || !s2)
+		return (1);
+	index = 0;
+	while (s1[index] && s2[index] && s1[index] == s2[index])
+		index++;
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+}
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	index;
 
-	if (!s1 || !s2 || n == 0)
+	if ((!s1 && !s2) || n == 0)
 		return (0);
+	if (!s1 || !s2)
+		return (1);
 	index = 0;
 	while (s1[index] && s2[index] && s1[index] == s2[index] && index < n - 1)
 		index++;
